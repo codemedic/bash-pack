5b4acda70b1076f6eee127fe8cd597932f959583
Dino Korah <dino.korah@redmatter.com>
2017-02-13 23:15:27 +0000

    Add more string utility functions
    replace_all (pure bash), and some validation functions

--- string.sh
+++ string.sh
@@ -1,3 +1,4 @@
+# to lower case
 to_lower() {
     if is_bash_version 4; then
         local str="$*"
@@ -7,6 +8,7 @@ to_lower() {
     fi
 }
 
+# to upper case
 to_upper() {
     if is_bash_version 4; then
         local str="$*"
@@ -16,6 +18,7 @@ to_upper() {
     fi
 }
 
+# prefix each line
 prefix_filter() {
     local prefix line
     [ $# -eq 0 ] || prefix="$*"
@@ -28,3 +31,35 @@ prefix_filter() {
     done
 }
 
+# Replace token with value, takes more than one token-value pair
+#
+# Usage:
+#    replace_string Token1 Value1 [ [Token2 Value2] ... ]
+replace_all() {
+    { [ $# -gt 0 ] && [ "$(($#%2))" -eq 0 ]; } || {
+        bail "replace_string: invalid invocation; variable name and values should be in pairs"
+    }
+
+    local input; input="$(</dev/stdin)"
+
+    while [ $# -gt 0 ]; do
+        # consume a variable-name and value pair and formulate sed pattern
+        local var="$1" val="$2"; shift 2
+        input="${input//"$var"/"$val"}"
+    done
+
+    echo -n "$input"
+}
+
+# Escape sed match pattern
+sed_escape_pattern() {
+    echo "$1" | sed -e 's/\([[\/.*]\|\]\)/\\&/g'
+}
+
+# Escape sed replace pattern
+sed_escape_replace() {
+    echo "$1" | sed -e 's/[\/&]/\\&/g'
+}
+
+# sed_escape* function usage example
+# sed "s/$(sed_escape_pattern "$var")/$(sed_escape_replace "$val")/g"
---
+++ validate.sh
@@ -0,0 +1,15 @@
+# Check if a string is fully qualified domain name
+is_fqdn() {
+    echo "$1" | grep -qP '(?=^.{4,253}$)(^(?:[a-zA-Z](?:(?:[a-zA-Z0-9\-]){,61}[a-zA-Z])?\.)+[a-zA-Z]{2,}$)'
+}
+
+# Check if a string is integer
+is_integer() {
+    [[ "$1" =~ ^[0-9]+$ ]]
+}
+
+# is a number (not just integer)
+is_number() {
+    [ "$1" -eq "$1" ] 2>/dev/null
+}
+
